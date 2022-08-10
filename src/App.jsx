2d9748bf603ccf37d18abbb5547d6843e8120928
SalihTpc <salihtopcu@hotmail.com.tr>
2022-08-10 19:56:46 +0300

    a page added for invalid route

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css";
 import "antd/dist/antd.css";
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+// import InValid from "./pages/InValid";
 // import Result from "./pages/Result";
 // import Home from "./pages/Home";
 const Home = lazy(() => import("./pages/Home"));
 const Result = lazy(() => import("./pages/Result"));
+const InValid = lazy(() => import("./pages/InValid"));
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/*" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/results" element={<Result />}></Route>
+        <Route path="/*" element={<InValid />} />
       </Routes>
     </Suspense>
   );
---
+++ src/pages/InValid.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InValid = () => {
+  return (
+    <div class="flex h-screen">
+      <h1 className="text-5xl m-auto text-red-600">
+        This page does not EXIST!!!
+      </h1>
+    </div>
+  );
+};
+
+export default InValid;
